57ecc8743de41fdec72b3a6f13ac47dc81cca450
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-09-22 11:36:06 +0200

    7-13: Remove elements

---
+++ 06-document-object-model/13-remove-elements/shopping-list/script.js
@@ -0,0 +1,44 @@
+function removeClearButton() {
+	const clearBtn = document.querySelector("#clear");
+	clearBtn.remove();
+}
+
+function removeFirstItem() {
+	const ul = document.querySelector("ul");
+	const li = document.querySelector("li:first-child");
+
+	ul.removeChild(li);
+}
+
+// function removeItem(itemNumber) {
+// 	const lis = document.querySelectorAll("li");
+// 	const ul = document.querySelector("ul");
+
+// 	lis.forEach((item, index) => (index + 1) === itemNumber ? ul.removeChild(item) : null);
+// }
+
+function removeItem(itemNumber) {
+	const ul = document.querySelector("ul");
+	const li = document.querySelector(`li:nth-child(${itemNumber})`);
+
+	ul.removeChild(li);
+}
+
+function removeItem2(itemNumber) {
+  const ul = document.querySelector("ul");
+  const li = document.querySelectorAll("li")[itemNumber - 1];
+
+  ul.removeChild(li);
+}
+
+function removeItem3(itemNumber) {
+	const li = document.querySelectorAll("li");
+	li[itemNumber - 1].remove();
+}
+
+const removeItem4 = (itemNumber) => 
+	document.querySelectorAll("li")[itemNumber - 1].remove();
+
+removeClearButton();
+//removeFirstItem();
+removeItem(1);
