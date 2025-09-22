0b644ab11a58c046d186a44b912bd30d1eadeadd
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-09-22 11:16:37 +0200

    7-12: Replace elements

---
+++ 06-document-object-model/12-replacing-elements/shopping-list/script.js
@@ -0,0 +1,48 @@
+function replaceFirstItem() {
+	const firstItem = document.querySelector("li:first-child");
+
+	const li = document.createElement("li");
+	li.textContent = "Replace first";
+
+	firstItem.replaceWith(li);
+}
+
+function replaceSecondItem() {
+	const secondItem = document.querySelector("li:nth-child(2)");
+	
+	secondItem.outerHTML = "<li>Replaced second</li>";
+	// OuterHTML incluye la propia etiqueta del elemento a reemplazar
+}
+
+function replaceAllItems() {
+	const lis = document.querySelectorAll("li"); // Node List
+
+	// lis.forEach((item, index) => {
+	// 	//item.outerHTML = "<li>Replace All</li>";
+	// 	if (index == 1)
+	// 		item.innerHTML = "Second Item";
+	// 	else
+	// 		item.innerHTML = "Replace All"
+	// });
+
+	lis.forEach((item, index) => item.outerHTML = index === 1 ? "<li>Second Item</li>" : "<li>Item</li>");
+}
+
+function replaceChildHeading() {
+	const header = document.querySelector("header");
+
+	// Selecciona un h1 descendiente de header
+	const h1 = document.querySelector("header h1");
+	
+	const h2 = document.createElement("h2");
+	h2.id = "app-title";
+	h2.textContent = "Shopping List";
+
+	header.replaceChild(h2, h1);
+
+}
+
+replaceFirstItem();
+replaceSecondItem();
+replaceAllItems();
+replaceChildHeading();
