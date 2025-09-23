f4d4e40da1c160aa0b2b1e82709668d96bc8c699
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-09-23 22:45:54 +0200

    8.3 - Event Object

--- 07-events/03-event-object/shopping-list/script.js
+++ 07-events/03-event-object/shopping-list/script.js
@@ -1,6 +1,47 @@
+const logo = document.querySelector("img");
+
+function onClick(e) {
+	// console.log(e.target);
+	// console.log(e.currentTarget);
+	// console.log(e.type);
+	// console.log(e.timeStamp);
+	// console.log(e.clientX);
+	// console.log(e.clientY);
+	// console.log(e.offsetX);
+  	// console.log(e.offsetY);
+	// console.log(e.screenX);
+  	// console.log(e.screenY);
+};
+
+logo.addEventListener("click", onClick);
+
+// document.body.addEventListener("click", function (e) {
+// 	console.log(e.target);
+//   	console.log(e.currentTarget);
+// })
+
+
+/* PREVENT DEFAULT*/
+// Evita que se realice la acción por defecto asociada a un evento
+
+document.querySelector("a").addEventListener("click", function(e) {
+	e.preventDefault();
+
+	console.log("Link was clicked");
+})
+
+function onDrag(e) {
+	document.querySelector("h1").textContent = `X ${e.clientX} Y ${e.clientY}`;
+}
+
+logo.addEventListener("drag", onDrag);
+
 /*
 - `target` - The element that triggered the event
 - `currentTarget` - The element that the event listener is attached to (These are the same in this case
+
+Podemos añadir un eventListener al body por ejemplo para detectar clicks. Entonces cuando se pulsa en un li por ejemplo, ese evento los dispara el li pero se realiza la acción por estar el eventListener asociado al body, ya que los eventos se elevan (bubbling), desde abajo hasta arriba
+
 - `type` - The type of event that was triggered
 - `timeStamp` - The time that the event was triggered
 - `clientX` - The x position of the mouse click relative to the window
