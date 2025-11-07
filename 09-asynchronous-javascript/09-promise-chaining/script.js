7ee724157fff33786dddc889e479852371724180
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-11-07 13:27:32 +0100

    Promise Chaining

--- 09-asynchronous-javascript/09-promise-chaining/script.js
+++ 09-asynchronous-javascript/09-promise-chaining/script.js
@@ -1,11 +1,11 @@
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    let error = false;
+    let error = true;
 
     if (!error) {
-      resolve({ name: 'John', age: 30 });
+      resolve({ name: "John", age: 30 });
     } else {
-      reject('Error: Something went wrong');
+      reject("Error: Something went wrong");
     }
   }, 1000);
 });
@@ -13,5 +13,17 @@ const promise = new Promise((resolve, reject) => {
 promise
   .then((user) => {
     console.log(user);
+    return user.name; // Este return pasa al siguiente then como argumento de la función
   })
-  .catch((error) => console.log(error))
+  .then((name) => {
+    console.log(name);
+    return name.length;
+  })
+  .then((nameLength) => {
+    console.log(nameLength);
+  })
+  .catch((error) => {
+    console.log(error);
+    return 123;
+  })
+  .then((x) => console.log("This will run no matter what", x));
