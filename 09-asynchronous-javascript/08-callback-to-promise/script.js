d01387ae30c23406b33f64f24bd3dc58b97cb0a2
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-11-07 13:21:01 +0100

    10-11 Callback to Promise refactor

--- 09-asynchronous-javascript/08-callback-to-promise/script.js
+++ 09-asynchronous-javascript/08-callback-to-promise/script.js
@@ -1,9 +1,11 @@
-
 const posts = [
-  { title: 'Post One', body: 'This is post one' },
-  { title: 'Post Two', body: 'This is post two' },
+  { title: "Post One", body: "This is post one" },
+  { title: "Post Two", body: "This is post two" },
 ];
 
+/*
+
+Using Callbacks 
 function createPost(post, cb) {
   setTimeout(() => {
     posts.push(post);
@@ -20,5 +22,39 @@ function getPosts() {
     });
   }, 1000);
 }
+*/
 
-createPost({ title: 'Post Three', body: 'This is post' }, getPosts);
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+		let error = false;
+		if (!error) {
+			posts.push(post);
+			resolve();
+		} else {
+			reject("Something went wrong");
+		}
+    }, 2000);
+  });
+}
+
+// Igual
+function getPosts() {
+  setTimeout(() => {
+    posts.forEach(function (post) {
+      const div = document.createElement("div");
+      div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+      document.querySelector("#posts").appendChild(div);
+    });
+  }, 1000);
+}
+
+function showError(error) {
+	const h3 = document.createElement("h3");
+	h3.innerHTML = `<strong>${error}</strong>`;
+	document.getElementById("posts").appendChild(h3);
+}
+
+createPost({ title: "Post Three", body: "This is post three" })
+	.then(getPosts)
+	.catch(showError);
